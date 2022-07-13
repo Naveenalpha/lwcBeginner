e024838e14adefe49fa35b1f7232f4be9366c020
naveenalpha <naveenredmi3s@gmail.com>
2022-07-13 13:57:20 +0530

    two way data binding

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,4 +1,15 @@
 import { LightningElement } from 'lwc';
 
-export default class HelloWorld extends LightningElement {}
+export default class HelloWorld extends LightningElement {
+    fullname = 'ZeroToHero'
+    users = ['a','b']
+    obj = {name:'naveen'}
+    handler(event){
+        this.fullname = event.target.value;
+    }
+    
+
+
+
+}
 
